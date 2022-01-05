a5a1e20a30ec9b4961a7e29b5987e208ef554e91
Felipe Refosco <luisfelipe.refosco@hotmail.com>
2022-01-05 04:43:56 -0300

    Create folders if don't have on Lutris default folders

--- osuUpdate.sh
+++ osuUpdate.sh
@@ -3,12 +3,16 @@
 echo "updating osu...";
 
 echo "Changing directory...";
+
+
+mkdir ~/Games ~/Games/osuLazer
+
 cd ~/Games/osuLazer/;
 
 echo "Downloading...";
 
 rm osu.AppImage
 
-wget -c --progress=dot --compression=auto https://github.com/ppy/osu/releases/latest/download/osu.AppImage;
+wget -c ls--compression=auto https://github.com/ppy/osu/releases/latest/download/osu.AppImage;
 
 chmod +x osu.AppImage
